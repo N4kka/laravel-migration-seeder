e830bc2dc28df8f1cae34ab84ed6b7284e1056e8
N4kka <sarlinicola7@gmail.com>
2022-06-30 10:09:42 +0200

    few little updates on Homepage Controller

--- app/Http/Controllers/HomepageController.php
+++ app/Http/Controllers/HomepageController.php
@@ -4,14 +4,16 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Train;
+use Carbon\Carbon;
 
 class HomepageController extends Controller
 {
-    public function index() {
+    public function index()
+    {
 
         $train = Train::all();
 
-        $today_train = Train::where('departure_date', '2022/06/29')->get();
+        $today_train = Train::where('departure_date', Carbon::today())->get();
 
         return view('Homepage', compact('train', 'today_train'));
     }
